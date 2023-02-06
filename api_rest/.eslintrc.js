73cd2d173dd2a6b469d8ea69836ad5fb467c2aa9
yanandreatta <yan.duca2@gmail.com>
2023-02-06 04:04:20 -0300

    Configurado e testado o Sequelize

--- api_rest/.eslintrc.js
+++ api_rest/.eslintrc.js
@@ -13,5 +13,9 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'class-methods-use-this': 'off',
+    'import/first': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/newline-after-import': 'off',
+    'eslint-disable-next-line': 'off',
   },
 };
--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoALuno = await Aluno.create({
+      nome: 'Maria',
+      sobrenome: 'Andreatta',
+      email: 'Maria@gmail.com',
+      idade: 23,
+      peso: 57,
+      altura: 1.7,
     });
+    res.json(novoALuno);
   }
 }
 
