0df9939f1886d741c87a1d704396716484aadf2d
yan <yan.duca2@gmail.com>
2023-02-16 15:09:19 -0300

    Adicionando tarefas - terminado

--- react/lista/src/components/Main.js
+++ react/lista/src/components/Main.js
@@ -12,6 +12,22 @@ export default class Main extends Component {
     tarefas: []
   };
 
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa]
+    });
+  };
+
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value
@@ -25,7 +41,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
