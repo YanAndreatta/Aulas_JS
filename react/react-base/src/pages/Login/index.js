9bcb47b65c48ab1d97ec69f4316586d605497e96
yan <yan.duca2@gmail.com>
2023-02-21 21:46:06 -0300

    Configurando as cores

--- react/react-base/src/pages/Login/index.js
+++ react/react-base/src/pages/Login/index.js
@@ -11,7 +11,7 @@ export default function Login() {
         <small>Oie</small>
       </Title>
       <Paragrafo>Lorem ipsum dolor sit, amet.</Paragrafo>
-      <a href="/">Oie</a>
+      <button type="button">Enviar</button>
     </Container>
   );
 }
