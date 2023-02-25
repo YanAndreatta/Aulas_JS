0ee0f779816a9e9a90bc1ed5b124ef588f7acd52
yan <yan.duca2@gmail.com>
2023-02-25 02:22:14 -0300

    Páginas e rotas da aplicação feitas

--- react/consumirapi/src/store/modules/example/reducer.js
+++ react/consumirapi/src/store/modules/example/reducer.js
@@ -8,19 +8,16 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case types.BOTAO_CLICADO_SUCCESS: {
-      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
 
     case types.BOTAO_CLICADO_FAILURE: {
-      console.log('Deu erro =(');
       return state;
     }
 
     case types.BOTAO_CLICADO_REQUEST: {
-      console.log('Estou fazendo a requisição');
       return state;
     }
     default:
