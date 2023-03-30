f0d9a1ebbae5558f94a2ece1d3206b3ca328b243
yan <yan.duca2@gmail.com>
2023-03-30 10:53:29 -0300

    login#2

--- react/consumirapi/src/store/modules/auth/sagas.js
+++ react/consumirapi/src/store/modules/auth/sagas.js
@@ -1,14 +1,35 @@
-// eslint-disable-next-line no-unused-vars
 import { call, put, all, takeLatest } from 'redux-saga/effects';
-// eslint-disable-next-line no-unused-vars
 import { toast } from 'react-toastify';
-// eslint-disable-next-line no-unused-vars
+import { get } from 'lodash';
 import * as actions from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
 
-// eslint-disable-next-line require-yield
 function* loginRequest({ payload }) {
-  console.log('SAGA', payload);
+  try {
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.success('Você fez login');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
+  } catch (e) {
+    toast.error('Usuário ou senha inválidos.');
+
+    yield put(actions.loginFailure());
+  }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+function persisRehydrate({ payload }) {
+  const token = get(payload, 'auth.token', '');
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persisRehydrate),
+]);
